8e7678c87c8033e69460a341b5e9b32a8671e2f9
PialKanti <pialkanti2012@gmail.com>
2023-03-07 12:02:28 +0600

    Added login and navbar components

--- AttendanceManagement.ClientApp/src/App.vue
+++ AttendanceManagement.ClientApp/src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NavBar />
+  <div class="container">
+    <LoginForm />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue';
+import LoginForm from './components/LoginForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavBar,
+    LoginForm
   }
 }
 </script>
@@ -19,8 +23,10 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+}
+
+.container {
   margin-top: 60px;
 }
 </style>
---
+++ AttendanceManagement.ClientApp/src/components/LoginForm.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="row">
+        <div class="col"></div>
+        <div class="col">
+            <form>
+                <div class="mb-3">
+                    <label for="username" class="form-label">Username</label>
+                    <input type="text" class="form-control" id="username" aria-describedby="usernameHelp">
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="password">
+                </div>
+                <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input" id="rememberCheckbox">
+                    <label class="form-check-label" for="rememberCheckbox">Remember me</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Login</button>
+            </form>
+        </div>
+        <div class="col"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LoginForm'
+}
+</script>
+
+<style scoped></style>
