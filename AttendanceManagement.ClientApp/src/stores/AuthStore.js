a2d13f8563b0f24f23fbbb2cd0b63843f16909bb
PialKanti <pialkanti2012@gmail.com>
2023-03-09 17:24:39 +0600

    Added AuthStore

---
+++ AttendanceManagement.ClientApp/src/stores/AuthStore.js
@@ -0,0 +1,20 @@
+import { defineStore } from "pinia";
+
+export const useAuthStore = defineStore('authStore', {
+    state: () => {
+        user: {
+            isLoggedIn: false;
+            username: '';
+        }
+    },
+    actions: {
+        login(username) {
+            this.user.isLoggedIn = true;
+            this.user.username = username;
+        },
+        logout() {
+            this.user.isLoggedIn = false;
+            this.user.username = '';
+        }
+    }
+})
