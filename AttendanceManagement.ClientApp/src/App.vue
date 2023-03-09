76e777ce24b7f736c437e4148af9ceb9af05a1aa
PialKanti <pialkanti2012@gmail.com>
2023-03-09 18:12:55 +0600

    Added dashboard

--- AttendanceManagement.ClientApp/src/App.vue
+++ AttendanceManagement.ClientApp/src/App.vue
@@ -1,19 +1,28 @@
 <template>
   <NavBar />
   <div class="container">
-    <LoginForm />
+    <DashBoard v-if="authStore.user.isLoggedIn" />
+    <LoginForm v-else />
   </div>
 </template>
 
 <script>
 import NavBar from './components/NavBar.vue';
+import DashBoard from './components/DashBoard.vue';
 import LoginForm from './components/LoginForm.vue'
+import { useAuthStore } from './stores/AuthStore';
 
 export default {
+  setup() {
+    const authStore = useAuthStore();
+
+    return { authStore };
+  },
   name: 'App',
   components: {
     NavBar,
-    LoginForm
+    LoginForm,
+    DashBoard
   }
 }
 </script>
---
+++ AttendanceManagement.ClientApp/src/components/DashBoard.vue
@@ -0,0 +1,8 @@
+<template>
+    <p>Welcome to dashboard</p>
+</template>
+<script>
+export default {
+    name: 'DashBoard'
+}
+</script>
