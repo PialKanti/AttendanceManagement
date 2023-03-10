1a480002974f8ae1f0476110e2b694dc5ffa1d6f
Pial Kanti Samadder <pialkanti2012@gmail.com>
2023-03-11 01:53:43 +0600

    Updated dashboard design

--- AttendanceManagement.ClientApp/src/components/DashBoard.vue
+++ AttendanceManagement.ClientApp/src/components/DashBoard.vue
@@ -1,8 +1,32 @@
 <template>
-    <p>Welcome to dashboard</p>
+    <div class="content">
+        <p class="text-center info-text">No data found for today.</p>
+        <div class="text-center report-button">
+            <button type="button" class="btn btn-success" @click="reportEntry">Report entry time</button>
+        </div>
+    </div>
 </template>
 <script>
 export default {
-    name: 'DashBoard'
+    name: 'DashBoard',
+    methods: {
+        reportEntry() {
+            const currentDate = Date.UTC();
+            console.log(currentDate);
+        }
+    }
 }
-</script>
+</script> 
+<style scoped> .content {
+     margin-top: 60px;
+ }
+
+ .info-text {
+     font-size: 13pt;
+     color: grey;
+ }
+
+ .report-button {
+     margin-top: 20px;
+ }
+</style>
