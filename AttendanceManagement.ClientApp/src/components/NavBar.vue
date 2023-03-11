e485e733e780589281191509faa850be174022e8
PialKanti <pialkanti2012@gmail.com>
2023-03-11 14:55:02 +0600

    Added logout navbar

--- AttendanceManagement.ClientApp/src/components/NavBar.vue
+++ AttendanceManagement.ClientApp/src/components/NavBar.vue
@@ -6,18 +6,29 @@
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0" v-if="authStore.user.isLoggedIn">
+            <div class="collapse navbar-collapse" id="navbarLoggedUser" v-if="authStore.user.isLoggedIn">
+                <ul class="navbar-nav mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <router-link to="/dashboard" active-class="active" class="nav-link"
+                            aria-current="page">Home</router-link>
                     </li>
                 </ul>
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0" v-else>
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <router-link to="/login" class="nav-link" aria-current="page">Login</router-link>
+                        <router-link to="/logout" active-class="active" class="nav-link"
+                            aria-current="page">Logout</router-link>
+                    </li>
+                </ul>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarDefault" v-else>
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <router-link to="/login" active-class="active" class="nav-link"
+                            aria-current="page">Login</router-link>
                     </li>
                     <li class="nav-item">
-                        <router-link to="/register" class="nav-link" aria-current="page">Register</router-link>
+                        <router-link to="/register" active-class="active" class="nav-link"
+                            aria-current="page">Register</router-link>
                     </li>
                 </ul>
             </div>
