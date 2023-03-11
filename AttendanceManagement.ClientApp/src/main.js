0c470e5a77998e0c0491ce9d440d3f48b93e9c62
PialKanti <pialkanti2012@gmail.com>
2023-03-11 12:58:08 +0600

    Implemented navigation guard for all routes

--- AttendanceManagement.ClientApp/src/main.js
+++ AttendanceManagement.ClientApp/src/main.js
@@ -4,10 +4,20 @@ import * as VueRouter from 'vue-router'
 import App from './App.vue'
 import { routes } from './routes/router'
 import './assets/main.css'
+import { useAuthStore } from './stores/AuthStore'
 
 const router = VueRouter.createRouter({
     history: VueRouter.createWebHistory(),
     routes
+})
+
+router.beforeEach((to) => {
+    const authStore = useAuthStore();
+    console.log(authStore.user.isLoggedIn);
+    if (!authStore.user.isLoggedIn && to.name !== 'Login') {
+        return { name: 'Login' };
+    }
+    return true;
 })
 
 createApp(App).use(createPinia()).use(router).mount('#app')
--- AttendanceManagement.ClientApp/src/routes/router.js
+++ AttendanceManagement.ClientApp/src/routes/router.js
@@ -2,6 +2,6 @@ import DashBoard from '@/components/DashBoard'
 import LoginForm from '@/components/LoginForm'
 
 export const routes = [
-    { path: '/dashboard', component: DashBoard },
-    { path: '/login', component: LoginForm },
+    { path: '/dashboard', name: 'Dashboard', component: DashBoard },
+    { path: '/login', name: 'Login', component: LoginForm },
 ];
