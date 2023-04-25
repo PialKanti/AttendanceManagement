648e8575f40a23bb9198a72ce7102f834412824f
Pial Kanti Samadder <pialkanti2012@gmail.com>
2023-04-25 21:54:30 +0600

    Added clean architecture project structure

---
+++ src/Core/Domain/AttendanceManagement.Domain/Entities/Attendance.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AttendanceManagement.Domain.Entities
+{
+    public class Attendance : BaseEntity
+    {
+        [Required]
+        public string UserId { get; set; }
+        [Required]
+        public long EntryTimestamp { get; set; }
+        public long? ExitTimestamp { get; set; }
+        [Required]
+        public DateTime EntryDate { get; set; }
+        [Required]
+        public int Month { get; set; }
+        [Required]
+        public int Year { get; set; }
+    }
+}
