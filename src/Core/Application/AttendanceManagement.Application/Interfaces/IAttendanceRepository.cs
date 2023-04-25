52f4cae7e7dc6a906d9a623aa54abbbf595d2604
Pial Kanti Samadder <pialkanti2012@gmail.com>
2023-04-25 22:45:14 +0600

    Added IAttendanceRepository interface to Application project

---
+++ src/Core/Application/AttendanceManagement.Application/Interfaces/IAttendanceRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace AttendanceManagement.Application.Interfaces
+{
+    public interface IAttendanceRepository<T>
+    {
+        Task<T> CreateAsync(T dtoModel, string userId);
+        Task UpdateAsync(string id, T attendance);
+        Task<T?> GetAsync(string id);
+        Task<IEnumerable<T>> GetUserMonthlyAttendancesAsync(string username, int month, int year);
+        //IEnumerable<MonthlyAttendanceGroupDto> GetUserYearlyAttendancesAsync(string username, int year);
+    }
+}
