bb7a67c07f28c52f9d5b1d083eb1d6df85692efa
Pial Kanti Samadder <pialkanti2012@gmail.com>
2023-04-26 00:57:25 +0600

    Updated Attendance repository interface

--- src/Core/Application/AttendanceManagement.Application/Interfaces/IAttendanceRepository.cs
+++ src/Core/Application/AttendanceManagement.Application/Interfaces/IAttendanceRepository.cs
@@ -2,10 +2,10 @@
 {
     public interface IAttendanceRepository<T>
     {
-        Task<T> CreateAsync(T dtoModel, string userId);
+        Task<T> CreateAsync(T attendance);
         Task UpdateAsync(string id, T attendance);
         Task<T?> GetAsync(string id);
-        Task<IEnumerable<T>> GetUserMonthlyAttendancesAsync(string username, int month, int year);
+        Task<IEnumerable<T>> GetUserMonthlyAttendancesAsync(string userId, int month, int year);
         //IEnumerable<MonthlyAttendanceGroupDto> GetUserYearlyAttendancesAsync(string username, int year);
     }
 }
